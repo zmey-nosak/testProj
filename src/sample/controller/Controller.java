98edd8a2114015c8b049d503cfedd9191648c5fe
zmey-nosak <zmey-nosak@yandex.ru>
2017-04-10 22:59:15 +0300

    add serfing directory

--- src/sample/controller/Controller.java
+++ src/sample/controller/Controller.java
@@ -1,28 +1,19 @@
 package sample.controller;
 
-import com.sun.glass.ui.View;
-import com.sun.xml.internal.ws.api.server.Container;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import lombok.SneakyThrows;
-import org.joda.time.DateTime;
-import org.w3c.dom.events.EventListener;
-import org.w3c.dom.events.MouseEvent;
 import sample.pojo.MyFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 import java.util.Arrays;
+import java.util.Optional;
 
 /**
  * Created by Stepan.Koledov on 10.04.2017.
@@ -36,6 +27,9 @@ public class Controller {
 
     @FXML
     private TableColumn<MyFile, String> nameColumn;
+
+    @FXML
+    private TableColumn<MyFile, String> isDirColumn;
 
     @FXML
     private TableColumn<MyFile, FileTime> createdDateColumn;
@@ -54,33 +48,39 @@ public class Controller {
             TableRow<MyFile> row = new TableRow<>();
             row.setOnMouseClicked(event -> {
                 if (event.getClickCount() == 2 && (!row.isEmpty())) {
-                    MyFile selectedRecord = (MyFile)tableUsers.getItems().get(row.getIndex());
-                    initData(new File(selectedRecord.getPath().toString()));
+                    MyFile selectedRecord = (MyFile) tableUsers.getItems().get(row.getIndex());
+                    if (selectedRecord.getIsDir().equals("D")) {
+                        initData(new File(selectedRecord.getPath().toString()));
+                    } else {
+                        Runtime r = Runtime.getRuntime();
+                        try {
+                            r.exec(selectedRecord.getPath().toString());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             });
             return row;
         });
+
         initData(new File("C:\\"));
 
-
-        // устанавливаем тип и значение которое должно хранится в колонке
         nameColumn.setCellValueFactory(new PropertyValueFactory<MyFile, String>("name"));
+        isDirColumn.setCellValueFactory(new PropertyValueFactory<MyFile, String>("isDir"));
         createdDateColumn.setCellValueFactory(new PropertyValueFactory<MyFile, FileTime>("createdDate"));
         modifiedDateColumn.setCellValueFactory(new PropertyValueFactory<MyFile, FileTime>("modifiedDateColumn"));
         sizeColumn.setCellValueFactory(new PropertyValueFactory<MyFile, Long>("size"));
-
-        // заполняем таблицу данными
         tableUsers.setItems(filesData);
     }
 
-    // подготавливаем данные для таблицы
-    // вы можете получать их с базы данных
-
-
     public void initData(File file) {
         filesData.clear();
-        if(file.getParentFile()!=null)
-            filesData.add(new MyFile());
+        if (file.getParentFile() != null) {
+            MyFile dir = new MyFile(file.getParentFile());
+            dir.setName("");
+            filesData.add(dir);
+        }
         Arrays.asList(file.listFiles()).stream().map(f -> {
             return new MyFile(f);
         }).forEach(e -> filesData.add(e));
--- src/sample/pojo/MyFile.java
+++ src/sample/pojo/MyFile.java
@@ -3,7 +3,7 @@ package sample.pojo;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
-import org.joda.time.DateTime;
+
 
 import java.io.File;
 import java.nio.file.Files;
@@ -18,15 +18,19 @@ import java.nio.file.attribute.FileTime;
 @Setter
 public class MyFile {
     private String name;
+    private File file;
     private Path path;
     private FileTime createdDate;
     private FileTime modifiedDate;
     private Long size;
+    private String isDir;
     private BasicFileAttributes basicFileAttributes;
 
     @SneakyThrows
     public MyFile(File file) {
+        this.file = file;
         name = file.getName();
+        isDir = file.isDirectory() == false ? "F" : "D";
         path = file.toPath();
         basicFileAttributes = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
         createdDate = basicFileAttributes.creationTime();
@@ -35,7 +39,4 @@ public class MyFile {
     }
 
 
-    public MyFile() {
-
-    }
 }
